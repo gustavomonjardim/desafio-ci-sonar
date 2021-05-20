61942318a2920ef08cd088879d145878ecb3feaf
Gustavo Monjardim <gustavomonjardim7@gmail.com>
2021-05-20 20:39:55 -0300

    feat: add functions and tests

---
+++ index.js
@@ -0,0 +1,22 @@
+const sum = (a, b) => {
+  return a + b;
+};
+
+const sub = (a, b) => {
+  return a - b;
+};
+
+const times = (a, b) => {
+  return a * b;
+};
+
+const divide = (a, b) => {
+  return a / b;
+};
+
+module.exports = {
+  sum,
+  sub,
+  times,
+  divide,
+};
---
+++ index.test.js
@@ -0,0 +1,17 @@
+const { sum, sub, times, divide } = require("./");
+
+test("should sum correctly", () => {
+  expect(sum(1, 1)).toBe(2);
+});
+
+test("should sub correctly", () => {
+  expect(sub(10, 1)).toBe(9);
+});
+
+test("should multiply correctly", () => {
+  expect(times(2, 3)).toBe(6);
+});
+
+test("should divide correctly", () => {
+  expect(divide(15, 3)).toBe(5);
+});
